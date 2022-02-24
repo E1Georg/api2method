90efc42e7e215f83e7998313f34856481de58259
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-02-24 13:41:17 +0300

    add models

---
+++ Models/ConvertPerson.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_T2.Models
+{
+    public class ConvertPerson
+    {
+        // Используется для получения данных с сайта(согласно заданию).
+        public string id { get; set; }
+        public string name { get; set; }
+        public string sex { get; set; }
+        public int Age { get; set; }
+    }
+}
