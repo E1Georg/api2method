e7aa0796955d46f2c88160a778c2d292e09a63c3
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-02-24 13:29:03 +0300

    add DBContext

---
+++ Data/MyDBContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+#nullable disable
+
+
+namespace API_T2.Data
+{
+    public partial class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+           Database.EnsureDeleted(); // UNIT TESTS
+           Database.EnsureCreated(); // При проведении тестирования необходимо закомментировать эти строки
+        }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<API_T2.Models.Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=myServer; Database=myDataBase;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
