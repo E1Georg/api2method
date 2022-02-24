1c8fe159647ca03624c3fd6a47f112dfbac94bb0
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-02-24 13:30:19 +0300

    upload Main()files

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
+using API_T2.Data;
+using API_T2.Models;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
-namespace Tele2
+
+namespace API_T2
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var data = getDataFromSite();
+            sendDataToBD(data);
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,5 +25,54 @@ namespace Tele2
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static List<Person> getDataFromSite()
+        {
+            var data = new List<Person>();
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(@"http://testlodtask20172.azurewebsites.net/task");
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "GET";          
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            // Получение списка жителей, но без возраста каждого жителя. Используется для получения id каждого жителя.
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var answer = streamReader.ReadToEnd();             
+                var result = JsonConvert.DeserializeObject<List<ConvertPerson>>(answer);                
+                string url;
+
+                foreach (var item in result)
+                {
+                    url = "http://testlodtask20172.azurewebsites.net/task/" + item.id;
+                    httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                    httpWebRequest.ContentType = "text/json";
+                    httpWebRequest.Method = "GET";
+                    httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                    // Получение списка жителей, но уже со всеми данными, с указанием возраста(по id). 
+                    using (var streamReaderFullList = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var answerWithFullData = streamReaderFullList.ReadToEnd();
+                        var resultWithFullData = JsonConvert.DeserializeObject<ConvertPerson>(answerWithFullData);
+                        data.Add(new Person { name = resultWithFullData.name, Age = resultWithFullData.Age, sex = resultWithFullData.sex });
+                    }                      
+                }  
+            }
+            return data;
+        }
+
+        public static void sendDataToBD(List<Person> data)
+        {          
+            using (MyDBContext context = new MyDBContext())
+            {   
+                foreach (var item in data)
+                {
+                    context.Persons.Add(item);
+                }
+                
+                context.SaveChanges();
+            }
+        }
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API_T2.Data;
 
-namespace Tele2
+
+namespace API_T2
 {
     public class Startup
     {
@@ -23,7 +21,8 @@ namespace Tele2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+            services.AddDbContext<MyDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDBContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,23 +32,15 @@ namespace Tele2
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
