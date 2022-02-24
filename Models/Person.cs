1a396eea2735237524910c1664c9dab7e9e5d926
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-02-24 13:34:16 +0300

    add model

---
+++ Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_T2.Models
+{
+    [Table("Persons")]
+    public class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sex { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {            
+            return this.name == ((Person)obj).name;
+        }
+    }
+}
